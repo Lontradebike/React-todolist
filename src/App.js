e9fde8d6388ca8a9fd4b45ca7b780c1176d6c9aa
lontradebike <jmarceloribeiro@hotmail.com>
2021-12-01 16:43:56 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const App = () => {
+    //let message = "hello world";
+    const [message, setMessage] = useState("Hello world")
+    return(
+      <>
+      <div className= "container">{message}
+
     </div>
-  );
-}
-
-export default App;
+   <button onClick={() => setMessage ("hellooooo")}> mudar mensagem
+   </button>
+   
+       </>
+     );
+  };
+  export default App; 
